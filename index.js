202cfd41f1ae952435b57d94280dd05aaaa5eb04
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-18 14:27:02 +0600

    5. Fetching a Single Movie

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const fetchData = async () => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "e9a5c027",
-      s: "avengers",
+      //   s: "avengers",
+      i: "tt0848228",
     },
   });
 
