c175b7d0e8b5fb5fc45bdac7aec73a2770fb746c
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-20 14:34:09 +0600

    30. Extracting Rendering Logic

--- autocomplete.js
+++ autocomplete.js
@@ -1,4 +1,4 @@
-const createAutocomplete = ({ root }) => {
+const createAutocomplete = ({ root, renderOption }) => {
   root.innerHTML = `
     <label><b>Search For a Movie</b></label>
     <input class='input' />
@@ -27,13 +27,9 @@ const createAutocomplete = ({ root }) => {
     dropdown.classList.add("is-active");
     for (let movie of movies) {
       const option = document.createElement("a");
-      const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
 
       option.classList.add("dropdown-item");
-      option.innerHTML = `
-        <img src="${imgSrc}" />
-        ${movie.Title}
-        `;
+      option.innerHTML = renderOption(movie);
       option.addEventListener("click", () => {
         dropdown.classList.remove("is-active");
         input.value = movie.Title;
